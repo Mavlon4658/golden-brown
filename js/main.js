d55a5baf84b7250b17aab4a325c47dd8570cf4d6
Mavlon4658 <mavlon4658@gmail.com>
2023-12-28 03:27:23 +0500

    desktop is ready

--- js/main.js
+++ js/main.js
@@ -30,16 +30,54 @@ lang_btn.onclick = () => {
     }
 }
 
+let main_selects = document.querySelectorAll('.main_select');
+if (main_selects.length) {
+    main_selects.forEach(select => {
+        let btn = select.querySelector('.main_select__btn');
+        let text = select.querySelector('.main_select__btn span');
+        let select_list = select.querySelector('.main_select__list_wrap');
+        let select_list_item = select.querySelectorAll('.main_select__list li');
+        btn.onclick = () => {
+            select_list.style.maxHeight = select_list.style.maxHeight ? null : select_list.scrollHeight + 'px';
+            select_list.classList.toggle('active')
+        }
+        select_list_item.forEach(list_item => {
+            list_item.onclick = () => {
+                select_list.style.maxHeight = select_list.style.maxHeight ? null : select_list.scrollHeight + 'px';
+                select_list.classList.remove('active')
+                text.textContent = list_item.textContent;
+                select_list_item.forEach(item => {
+                    item.classList.remove("active");
+                })
+                list_item.classList.add('active')
+            }
+        })
+    })
+}
+
 const target = document.querySelector('header .lang')
+
 
 document.addEventListener('click', (event) => {
     const withinBoundaries = event.composedPath().includes(target)
 
-    if (! withinBoundaries) {
+    if (!withinBoundaries) {
         if (lang_list.classList.contains('active')) {
             lang_list.classList.remove('active');
             lang_list.classList.add('no_active');
         }
+    }
+
+    if (main_selects.length) {
+        main_selects.forEach(select => {
+            let condition = event.composedPath().includes(select);
+            let select_list = select.querySelector('.main_select__list_wrap');
+            if (!condition) {
+                if (select_list.classList.contains('active')) {
+                    select.querySelector('.main_select__btn').click();
+                }
+            }
+        })
     }
 })
 
@@ -49,7 +87,7 @@ if (apartments_card) {
         slidesPerView: 2.3,
         spaceBetween: 48,
     })
-    
+
     let apartments_cards__ins = document.querySelectorAll('.apartments .apartments_cards__in');
     let apartments_cards__in_paginations = document.querySelectorAll('.apartments .apartments_cards__in_pagination');
     if (apartments_cards__ins.length) {
@@ -74,6 +112,37 @@ if (apartments_card) {
     }
 }
 
+let top_object_card = document.querySelector('.top_object .apartments_cards')
+if (top_object_card) {
+    top_object_slider = new Swiper(top_object_card, {
+        slidesPerView: 2.3,
+        spaceBetween: 48,
+    })
+
+    let top_objects = document.querySelectorAll('.top_object .apartments_cards__in');
+    let top_object_paginations = document.querySelectorAll('.top_object .apartments_cards__in_pagination');
+    if (top_objects.length) {
+        top_objects.forEach((card, idx) => {
+            let slide = new Swiper(card, {
+                slidesPerView: 1,
+                spaceBetween: 0,
+                pagination: {
+                    el: top_object_paginations[idx],
+                    clickable: true
+                }
+            })
+
+            slide.on('touchStart', function () {
+                top_object_slider.detachEvents();
+            });
+
+            slide.on('touchEnd', function () {
+                top_object_slider.attachEvents();
+            });
+        })
+    }
+}
+
 let stars = document.querySelectorAll('.star');
 if (stars.length) {
     stars.forEach(star => {
@@ -82,3 +151,58 @@ if (stars.length) {
         }
     })
 }
+
+function formatAndAddDollarSign(inputElement) {
+    let numericRegex = /^[0-9.]+$/;
+    let inputValue = inputElement.value;
+    if (numericRegex.test(inputValue)) {
+        let formattedValue = inputValue.replace(/\D/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+        inputElement.value = formattedValue + ' $';
+    } else {
+        inputElement.value = inputValue.replace(/[^\d.]/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + ' $';
+    }
+}
+
+function handleBackspace(inputElement, event) {
+    if (event.key === 'Backspace') {
+        let lastChar = inputElement.value.slice(-1);
+
+        if (lastChar === '.' && !inputElement.value.includes('.')) {
+            inputElement.value = inputElement.value.slice(0, -1);
+        } else {
+            inputElement.value = inputElement.value.replace(/[^\d.]/g, '');
+        }
+    }
+}
+
+let init = false;
+let approach_card_slider;
+let approach_card = document.querySelector('.approach .approach_card');
+
+if (approach_card) {
+    function swiperCard() {
+        if (window.innerWidth <= 992) {
+            if (!init) {
+                init = true;
+                approach_card_slider = new Swiper(approach_card, {
+                    slidesPerView: 'auto',
+                    spaceBetween: 18,
+                    scrollbar: {
+                        el: ".catalog_card__paginate",
+                    },
+                });
+            }
+        } else if (init) {
+            approach_card_slider.destroy();
+            init = false;
+        }
+    }
+    swiperCard();
+}
+
+window.addEventListener("resize", function () {
+    if (approach_card) {
+        swiperCard();
+    }
+});
+
